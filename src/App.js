ffaf5b1b946af6c9a12f340a402dc0b3ea4b06e1
Hubert Jo Sumarno <hubertjosumarno@gmail.com>
2023-09-22 01:12:14 +0100

    Added h1

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h1>This is hosted on NETLIFY</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
